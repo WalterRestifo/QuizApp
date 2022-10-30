a6c06e650ebb861049aa5a6ecfa6d8e98c36dcc9
Walter Restifo <walterrestifo93@gmail.com>
2022-10-30 09:30:42 +0100

    add bookmarks buttons and a img when bookmarks-page is empty

--- Quiz-app/index.js
+++ Quiz-app/index.js
@@ -1,7 +1,25 @@
-let bodyElement = document.querySelector('[data-js = "body"]');
-
+const bodyElement = document.querySelector('[data-js = "body"]');
 const darkModeSwitch = document.querySelector('[data-js = "darkModeSwitch"]');
+
+const spoilerButton1 = document.querySelector('[data-js = "spoiler-button-1"]');
+const spoilerButton2 = document.querySelector('[data-js = "spoiler-button-2"]');
+const answer1 = document.querySelector('[data-js = "answer-1"]');
+const answer2 = document.querySelector('[data-js = "answer-2"]');
 
 darkModeSwitch.addEventListener("click", () => {
   bodyElement.classList.toggle("dark-mode");
+});
+
+spoilerButton1.addEventListener("click", () => {
+  if (answer1.classList.contains("hidden")) {
+    answer1.classList.remove("--hidden");
+    answer1.classList.add("--not-hidden");
+  } else {
+    answer1.classList.remove("--hidden");
+    answer1.classList.add("--hidden");
+  }
+});
+
+spoilerButton2.addEventListener("click", () => {
+  answer2.classList.toggle("hidden");
 });
