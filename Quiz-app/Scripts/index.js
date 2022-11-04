52334e21ea8cdcf3904b3da20057fbdcfa77c0eb
Walter Restifo <walterrestifo93@gmail.com>
2022-11-04 12:02:33 +0100

    add form html,css and js files. Add a few images

--- Quiz-app/Scripts/index.js
+++ Quiz-app/Scripts/index.js
@@ -10,53 +10,45 @@ const bookmarkColorToChange2 = document.getElementById(
   "bookmark--color-to-change-2"
 );
 
-const bookmarkButton1 = document.querySelectorAll(
+const bookmarkButton1 = document.querySelector(
   '[data-js = "bookmark-button-1"]'
 );
-const bookmarkButton2 = document.querySelectorAll(
+const bookmarkButton2 = document.querySelector(
   '[data-js = "bookmark-button-2"]'
-);
-const cardNoBookmarks = document.querySelector(
-  '[data-js = "card--no-bookmarks"]'
-);
-const cardBookmarkPage1 = document.querySelector(
-  '[data-js = "card-bookmark-page-1"]'
-);
-const cardBookmarkPage2 = document.querySelector(
-  '[data-js = "card-bookmark-page-2"]'
 );
 
 spoilerButton1.addEventListener("click", () => {
-  if (answer1.classList.contains("hidden")) {
+  if (answer1.classList.contains("--hidden")) {
     answer1.classList.remove("--hidden");
-    answer1.classList.add("--not-hidden");
+    spoilerButton1.textContent = "Hide Answer";
   } else {
-    answer1.classList.remove("--hidden");
     answer1.classList.add("--hidden");
+    spoilerButton1.textContent = "Show Answer";
   }
 });
 
-spoilerButton2.addEventListener("click", () => {
-  answer2.classList.toggle("--hidden");
-  console.log("spoiler");
+spoilerButton2.addEventListener("click", (e) => {
+  if (answer2.classList.contains("--hidden")) {
+    answer2.classList.remove("--hidden");
+    e.target.textContent = "Hide Answer";
+  } else {
+    answer2.classList.add("--hidden");
+    e.target.textContent = "Show Answer";
+  }
 });
 
-//bookmarsks buttons don´t work. Probably for the same reason
-// why the dark mode switch works only for the profile page.
-// I have to save this changes in the style somewhere, or the page will
-// load new every time I use the navbar and reload the normal classes.
-bookmarkButton1.addEventListener("click", () => {
-  cardNoBookmarks.classList.add("--hidden");
-  cardBookmarkPage1.classList.toggle("--hidden");
+bookmarkButton1.addEventListener("click", (e) => {
+  if (e.target.getAttribute("src") === "Assets/bookmark-white.svg") {
+    e.target.setAttribute("src", "Assets/bookmark-black.svg");
+  } else {
+    e.target.setAttribute("src", "Assets/bookmark-white.svg");
+  }
 });
-// I need to find a way to know if the the bookmark on index is white or
-// black. I could then use the innerHTML method to change the
-// img in the bookmark button in a if-else conditional.
-// If not possible, I could declare a variable with beginning value = 0
-// then if (x % 2 == 0) do this and x++. So I know if the
-// button clicking is odd or even.
 
-bookmarkButton2.addEventListener("click", () => {
-  cardNoBookmarks.classList.add("--hidden");
-  cardBookmarkPage2.classList.toggle("--hidden");
+bookmarkButton2.addEventListener("click", (e) => {
+  if (e.target.getAttribute("src") === "Assets/bookmark-white.svg") {
+    e.target.setAttribute("src", "Assets/bookmark-black.svg");
+  } else {
+    e.target.setAttribute("src", "Assets/bookmark-white.svg");
+  }
 });
