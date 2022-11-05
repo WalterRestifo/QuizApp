d45d1598f72cde885bf5003ce505f74968530091
Walter Restifo <116001970+WalterRestifo@users.noreply.github.com>
2022-11-05 15:16:57 +0100

    Feature/buttons interaction (#5)
    * add bookmarks buttons and a img when bookmarks-page is empty
    
    * add commentary about my failed tasks for colleagues
    
    * forgot to save a document and now I have to push again
    
    * try to fix the buttons after a review
    
    * split js file
    
    * add form html,css and js files. Add a few images
    
    * add animation and style

--- Quiz-app/index.js
+++
@@ -1,7 +0,0 @@
-let bodyElement = document.querySelector('[data-js = "body"]');
-
-const darkModeSwitch = document.querySelector('[data-js = "darkModeSwitch"]');
-
-darkModeSwitch.addEventListener("click", () => {
-  bodyElement.classList.toggle("dark-mode");
-});
